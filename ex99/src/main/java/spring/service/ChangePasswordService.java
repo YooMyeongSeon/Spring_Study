15ee32d8be4aa4b0a75e2d5c3022e0d9a669f82d
YooMyeongSeon <s.cnelly@hotmail.com>
2022-07-04 16:29:17 +0900

    2022.07.04 commit

---
+++ ex99/src/main/java/spring/service/ChangePasswordService.java
@@ -0,0 +1,27 @@
+package spring.service;
+
+import spring.dao.MemberDao;
+import spring.exception.MemberNotFoundException;
+import spring.vo.Member;
+
+public class ChangePasswordService {
+//	private MemberDao dao = new MemberDao();
+	
+	private MemberDao dao;
+	
+	public ChangePasswordService(MemberDao memberDao) { //생성자로 의존 객체를 주입
+		this.dao = memberDao;
+	}
+	
+	public void changePassword(String email, String oldPassword, String newPassword) {
+		Member member = dao.selectByEmail(email);
+		
+		if (member == null) {
+			throw new MemberNotFoundException();
+		}
+		
+		member.changePassword(oldPassword, newPassword);
+		
+		dao.updateMember(member);
+	}
+}
